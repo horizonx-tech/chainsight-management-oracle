93acdf67a55754eb12571ecb3e25da207e171908
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-10-19 15:34:31 +0900

    update abi

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-abi-exporter';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.21",
+  solidity: "0.8.21",  
+  abiExporter: {
+    path: './abi',
+    format: 'json',
+    runOnCompile: true,
+    only: ['contracts/interfaces/*'],
+    flat: true,
+  },
 };
 
 export default config;
