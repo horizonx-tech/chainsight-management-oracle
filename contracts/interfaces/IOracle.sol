d7766529e047117e95bc8bc876c66fe179b209e6
darroze <darroze@horizonx.tech>
2023-10-20 16:02:34 +0900

    index sender

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 interface IOracle {
     function updateState(bytes calldata data) external;
-    event StateUpdated(address sender, bytes data);
+    event StateUpdated(address indexed sender, bytes data);
     function readAsString(address sender) external view returns (string memory);
     function readAsUint256(address sender) external view returns (uint256);
     function readAsUint128 (address sender) external view returns (uint128);
