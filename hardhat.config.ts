fc81598807165cc5682ba90505cd3773a1b4f359
hide-yoshi <hide-yoshi@users.noreply.github.com>
2024-01-10 11:24:28 +0900

    update env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,61 @@
+
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import 'hardhat-abi-exporter';
+import "hardhat-abi-exporter";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.21",  
+  solidity: "0.8.21",
   abiExporter: {
-    path: './abi',
-    format: 'json',
+    path: "./abi",
+    format: "json",
     runOnCompile: true,
-    only: ['contracts/interfaces/*'],
+    only: ["contracts/interfaces/*"],
     flat: true,
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      scrollSepolia: process.env.SCROLLSCAN_API_KEY || "",
+    },
+    customChains: [
+      {
+        network: "scrollSepolia",
+        chainId: 534351,
+        urls: {
+          apiURL: "https://api-sepolia.scrollscan.com/api",
+          browserURL: "https://sepolia.scrollscan.dev/",
+        },
+      },
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://sepolia.etherscan.io/api",
+          browserURL: "https://sepolia.etherscan.io/",
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10,
+      },
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: "https://endpoints.omniatech.io/v1/eth/sepolia/public",
+    },
+    scrollSepolia: {
+      chainId: 534351,
+      url: "https://rpc.ankr.com/scroll_sepolia_testnet",
+    },
   },
 };
 
